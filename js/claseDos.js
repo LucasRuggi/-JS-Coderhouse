378cd47aa532810cff929406bfa7e9accc792f3e
LucasRuggi <RuggirelloLucas22@gmail.com>
2021-06-26 14:36:10 -0300

    Se realizo el ejercicio de la clase 2

---
+++ js/claseDos.js
@@ -0,0 +1,18 @@
+let num1;
+let palabra;
+
+
+num1 = Number(prompt("Ingrese un numero:"));
+
+
+
+if (num1 > 1000) {
+    alert("El numero ingresado es mayor a 1000");
+} else if (num1 > 10 && num1 < 50) {
+    alert("El numero ingresado esta entre 10 y 50")
+}else if(num1 === 100){
+    alert("El numero ingresado es igual a 100")
+}
+
+
+
